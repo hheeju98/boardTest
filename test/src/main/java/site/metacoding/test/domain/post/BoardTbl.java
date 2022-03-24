4f5ecf5ec891fc5497197e655a7cb62bae6c12b7
hheeju98 <hheeju98@naver.com>
2022-03-24 15:37:35 +0900

    시험끝2

--- test/src/main/java/site/metacoding/test/domain/post/BoardTbl.java
+++ test/src/main/java/site/metacoding/test/domain/post/BoardTbl.java
@@ -7,10 +7,9 @@ import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
